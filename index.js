21977f7f7c8b724c1b0ad16810726b9dbcf09ec6
Moztanku <Mostankpl@gmail.com>
2021-06-03 17:08:44 +0200

    Fourth

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
-
+app.use(express.static(__dirname+'/html-css/Strona 2/'))
 app.get('/',(req,res)=>{
-    res.send('Hello world!');
+    res.sendFile(__dirname+'/html-css/Strona 2/index.html');
+});
+
+app.listen(3000,"localhost",()=>{
+    console.log("Listening on port 3000...");
 });
